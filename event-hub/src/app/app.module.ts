a1de554b8f52e72d7e438edfc9284270e10c9151
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-03-23 17:20:41 +0530

    added reusable event service

--- event-hub/src/app/app.module.ts
+++ event-hub/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { NavBarComponent } from './nav/nav-bar.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
