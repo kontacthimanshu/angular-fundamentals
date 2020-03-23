fe0a14f599d8a67e837ff8aa97629a93e138f1f9
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-03-23 16:37:37 +0530

    introduced ngClass to style elements

--- event-hub/src/app/events/event-thumbnail.component.ts
+++ event-hub/src/app/events/event-thumbnail.component.ts
@@ -4,6 +4,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     selector:'event-thumbnail',
     templateUrl:'./event-thumbnail.component.html',
     styles:[`
+        .green { color:#003300 !important; }
+        .bold { font-weight:bold; }
         .thumbnail { min-height: 210px; padding-left: 10px; background-color:#343a40; margin-bottom:10px; }
         .pad-left { margin-left: 10px; }
         .well div { color: #bbb; }
@@ -13,4 +15,10 @@ export class EventThumbnailComponent
 {
     @Input() event:any;
     @Output() eventClick = new EventEmitter();
+
+    getStartTimeClass()
+    {
+        const isEarlyStart = this.event && this.event.time === "8:00 am";
+        return {green: isEarlyStart, bold:isEarlyStart};
+    }
 }
