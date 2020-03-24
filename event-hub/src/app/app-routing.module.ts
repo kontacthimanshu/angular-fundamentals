a9319d9121f347330d59d954a4c132856b17c9c2
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-03-24 23:23:11 +0530

    Added lazily loaded user module

--- event-hub/src/app/app-routing.module.ts
+++ event-hub/src/app/app-routing.module.ts
@@ -13,7 +13,8 @@ const routes: Routes = [
   {path:'events/new', component:CreateEventComponent, canDeactivate:['canDeactivateCreateEvent']},
   {path:'events/:id', component: EventDetailsComponent, canActivate:[EventRouteActivatorService]},
   {path:'404', component:Error404Component},
-  {path:'', redirectTo:'/events',pathMatch:'full'}
+  {path:'', redirectTo:'/events',pathMatch:'full'},
+  {path:'user', loadChildren: () => import(`./user/user.module`).then(m => m.UserModule)}
 ];
 
 @NgModule({
