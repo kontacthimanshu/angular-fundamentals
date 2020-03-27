3cceddebc6fcf3f6dc8c8d2a06ce91f017253e91
kontacthimanshu@hotmail.com <kontacthimanshu@hotmail.com>
2020-03-27 18:34:05 +0530

    close the modal on click of session link

--- event-hub/src/app/common/simple-modal.component.ts
+++ event-hub/src/app/common/simple-modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, Inject } from '@angular/core';
+import { JQ_TOKEN } from './jquery.service';
 
 @Component({
     selector:'simple-modal',
     template:`
-        <div id="simple-modal" class="modal fade" tabindex="-1">
+        <div #containerRef id="simple-modal" class="modal fade" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -13,7 +14,7 @@ import { Component, Input } from '@angular/core';
                         </button>
                         
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body" (click)="closeModal()">
                         <ng-content></ng-content>
                     </div>
                 </div>
@@ -30,4 +31,15 @@ import { Component, Input } from '@angular/core';
 export class SimpleModalComponent
 {
     @Input() title:string;
+    @ViewChild('containerRef') containerEl:ElementRef;
+
+    constructor(@Inject(JQ_TOKEN) private $:any){
+
+    }
+
+    closeModal()
+    {
+        this.$(this.containerEl.nativeElement).modal('hide');
+    }
+
 }
